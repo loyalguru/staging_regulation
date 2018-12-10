e2042804a4f5b16e46963ff2be8bf80ec7ea5027
Eric Ponce <tricokun@gmail.com>
2018-12-10 11:49:44 +0100

    Fix: apply only when base is master

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ main(){
     action=$(jq --raw-output .action ${GITHUB_EVENT_PATH})
     merged=$(jq --raw-output .pull_request.merged ${GITHUB_EVENT_PATH})
     branch=$(jq --raw-output .pull_request.head.ref ${GITHUB_EVENT_PATH})
+    base=$(jq --raw-output .pull_request.base.ref ${GITHUB_EVENT_PATH})
 
     echo "DEBUG branch: ${branch} action: ${action} merged: ${merged}"
 
-    if [[ ! "$branch" == "staging" ]] && [[ "$action" == "closed" ]] && [[ "$merged" == "true" ]]; then
+    if [[ ! "$branch" == "staging" ]] && [[ "$base" == "master" ]] && [[ "$action" == "closed" ]] && [[ "$merged" == "true" ]]; then
         title=$(jq --raw-output .pull_request.title ${GITHUB_EVENT_PATH})
         echo "DEBUG {\"title\":\"${title}\", \"head\":\"${branch}\", \"base\": \"staging\"}"
         echo "Creating Pull request for ${GITHUB_REPOSITORY}"
